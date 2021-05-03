e3c6affa8a853721287469d4cd4f531fb7d1bf03
it19075136_Avantha <it19075136@my.sliit.lk>
2021-05-03 18:53:06 +0530

    Adding service changes

--- Rest-service/server.js
+++ Rest-service/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const userRoutes = require('./routes/userRoutes');
 
 const app = express(); // creating express app
 
 app.use(bodyParser.json()); // to accept json type requests
+
+app.use('/user', userRoutes) //user routes - avantha
+
+// product routes - pasindu
+
+// order/cart routes -thisara
+
+// orderHistory routes 
+
+// delivery routes - dilmika
+
+// payment routes - dilmika
 
 app.use('/',(req,res) => { // setting router
     res.json("Hi people");
