604b2e77db28b55535665bf4327e188e044e617f
it19020822_Thisara <it19020822@my.sliit.lk>
2021-05-11 22:11:14 +0530

    final css

--- web/src/Pages/CartPage.js
+++ web/src/Pages/CartPage.js
@@ -1,9 +1,24 @@
+import CartItem from '../components/CartItem'
 import './CartPage.css'
 
 const CartPage = () => {
     return (
         <div className="cartpage">
-            Cart Page
+            <div className="cartpage__left">
+                <h2>Shopping Cart</h2>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+            </div>
+            <div className="cartpage__right">
+                <div className="cartpage__info">
+                    <p>Subtotal (0) items</p>
+                    <p>$100.88</p>
+                </div>
+                <div className="btn__checkout">
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
