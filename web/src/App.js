32e2aab439063531de51165724c300ef59d776ae
IT19075204_GGDD_Weerasinghe <it19075204@my.sliit.lk>
2021-05-11 20:22:09 +0530

    changes

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter,Route } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index'
 import userRegistration from './components/userRegistration'
@@ -10,6 +11,8 @@ import HomePage from './Pages/HomePage';
 import CartPage from './Pages/CartPage';
 import Orders from './Pages/Orders'
 import Navbar from './components/Navbar';
+import Delivery from './Pages/DeliveryPage';
+import EditDelivery from './Pages/EditDelivery';
 
 const initstate = {}
 
@@ -31,6 +34,10 @@ function App() {
         <Route exact path="/product/:id" component={ProductPage} />
         <Route exact path="/cart" component={CartPage} />
         <Route exact path="/order" component={Orders}/>
+        <Route exact path="/delivery" component={Delivery}/>
+        <Route exact path="/delivery/edit" component={EditDelivery}/>
+
+
 
       </BrowserRouter>
     </Provider>
---
+++ web/src/Pages/DeliveryPage.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+
+class Delivery extends Component {
+    constructor(props){
+        super(props);
+
+        this.onChangeAmount = this.onChangeAmount.bind(this);
+        this.onChangeDelivery = this.onChangeDelivery.bind(this);
+        this.onChangeQuantity = this.onChangeQuantity.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            quantity : '',
+            amount : '',
+            deliveryItems : [],
+            isCancel : false
+        }
+    }
+
+    onChangeAmount(e){
+        this.setState({
+            amount: e.target.value
+        })
+    }
+
+    onChangeDelivery(e){
+        this.setState({
+            deliveryItems: e.target.value
+        })
+    }
+
+    onChangeQuantity(e){
+        this.setState({
+            quantity: e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const deliveryArray = this.state.deliveryItems.split(',');
+        const delivery  = {
+            quantity : this.state.quantity,
+            amount : this.state.amount,
+            deliveryItems : deliveryArray,
+            isCancel : false
+        }
+        console.log('delivery: ', delivery);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create a Delivery</h3>
+                <form onSubmit={this.onSubmit} className="container">
+                
+                     <div className="form-group">
+                            <label>Quantity</label>
+                            <input type="text"
+                                pattern="[0-9]*"
+                                required
+                                className="form-control"
+                                // value={this.state.quantity}
+                                onChange={this.onChangeQuantity}
+                            />
+                    </div>
+
+                    <br></br>
+                    <div className="form-group">
+                            <label>Enter Delivery Items</label>
+                            <input type="text"
+                                required
+                                placeholder="item1,item2,item3"
+                                className="form-control"
+                                // value={this.state.delivery}
+                                onChange={this.onChangeDelivery}
+                            />
+                    </div>
+
+                    <br></br>
+                    <div className="form-group">
+                            <label>Enter the Amount</label>
+                            <input type="text"
+                                pattern="[0-9]*"
+                                required
+                                className="form-control"
+                                // value={this.state.amount}
+                                onChange={this.onChangeAmount}
+                            />
+                    </div>    
+                    <br></br>
+                    <div className="form-group">
+                        <input type="submit" value="Create Delivery" className="btn btn-primary" />
+                    </div>
+                </form>
+ 
+
+                
+            </div>
+        );
+    }
+}
+
+export default Delivery;
