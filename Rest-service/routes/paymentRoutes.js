2c94bd7a1106417e1057064ef52b1ee5d9106d1c
IT19075204_GGDD_Weerasinghe <it19075204@my.sliit.lk>
2021-05-23 03:19:19 +0530

    sms

--- Rest-service/routes/paymentRoutes.js
+++ Rest-service/routes/paymentRoutes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 let Payment = require('../models/paymentModel');
 var nodemailer = require('nodemailer');
+const Vonage = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: "be899791",
+  apiSecret: "9IjSZjdwGGsrW760"
+})
 
 
 //add payment
@@ -10,6 +16,24 @@ router.post("/add", (req,res) => {
     console.log('newPayment: ', newPayment);
 
     newPayment.save().then(() => res.json("Payment Added!"));
+
+    const from = "Vonage APIs";
+    const to = "94776486255";
+    const text = "A text message sent using the Vonage SMS API";
+
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (responseData.messages[0]["status"] === "0") {
+          console.log("Message sent successfully.");
+        } else {
+          console.log(
+            `Message failed with error: ${responseData.messages[0]["error-text"]}`
+          );
+        }
+      }
+    });
 
         const email = req.body.userMail;
         const subject = 'Payment Verfication';
